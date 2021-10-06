aee5d13153f72458a916ff12eec1a654ad6ebe34
Tusharshrivastav <ts3657@gmail.com>
2021-10-06 07:48:39 +0530

    added header

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <template>
   <div>
-    <div class="app flex">
+    <div v-if="!mobile" class="app flex flex-column">
       <Navigation />
       <div class="app flex flex-column">
         <router-view />
       </div>
+    </div>
+     <div v-else class="mobile-message flex flex-column">
+      <h2>Sorry, this app is not supported on Mobile Devices</h2>
+      <p>To use this app, please use a computer or Tablet</p>
     </div>
   </div>
 </template>
@@ -12,23 +16,71 @@
 <script>
 import Navigation from "./components/Navigation.vue";
 export default {
+  data() {
+    return {
+      mobile: null,
+    };
+  },
   components: {
     Navigation,
+  },
+  created() {
+    this.checkScreen();
+    window.addEventListener("resize", this.checkScreen);
+  },
+  methods: {
+    checkScreen() {
+      const windowWidth = window.innerWidth;
+      if (windowWidth <= 750) {
+        this.mobile = true;
+        return;
+      }
+      this.mobile = false;
+    },
   },
 };
 </script>
 
 <style lang="scss">
 @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap");
-
 * {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
   font-family: "Poppins", sans-serif;
-  background-color: #141625;
 }
-
+.app {
+  background-color: #141625;
+  min-height: 100vh;
+  @media (min-width: 900px) {
+    flex-direction: row !important;
+  }
+  .app-content {
+    padding: 0 20px;
+    flex: 1;
+    position: relative;
+  }
+}
+.mobile-message {
+  text-align: center;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #141625;
+  color: #fff;
+  p {
+    margin-top: 16px;
+  }
+}
+// animated invoice
+.invoice-enter-active,
+.invoice-leave-active {
+  transition: 0.8s ease all;
+}
+.invoice-enter-from,
+.invoice-leave-to {
+  transform: translateX(-700px);
+}
 button,
 .button {
   cursor: pointer;
@@ -39,55 +91,42 @@ button,
   margin-right: 8px;
   color: #fff;
 }
-
 .dark-purple {
   background-color: #252945;
 }
-
 .red {
   background-color: #ec5757;
 }
-
 .purple {
   background-color: #7c5dfa;
 }
-
 .green {
   background-color: #33d69f;
 }
-
 .orange {
   background-color: #ff8f00;
 }
-
 // utility classes
-
 .flex {
   display: flex;
 }
-
 .flex-column {
   flex-direction: column;
 }
-
 .container {
   width: 100%;
   padding: 40px 10px;
   max-width: 850px;
   margin: 0 auto;
-
   @media (min-width: 900px) {
     padding-top: 72px;
   }
 }
-
 .nav-link {
   text-decoration: none;
   color: initial;
 }
-
 // Status Button Styling
-
 .status-button {
   &::before {
     content: "";
@@ -102,7 +141,6 @@ button,
   padding: 8px 30px;
   border-radius: 10px;
 }
-
 .paid {
   &::before {
     background-color: #33d69f;
@@ -110,7 +148,6 @@ button,
   color: #33d69f;
   background-color: rgba(51, 214, 160, 0.1);
 }
-
 .pending {
   &::before {
     background-color: #ff8f00;
@@ -118,7 +155,6 @@ button,
   color: #ff8f00;
   background-color: rgba(255, 145, 0, 0.1);
 }
-
 .draft {
   &::before {
     background-color: #dfe3fa;
