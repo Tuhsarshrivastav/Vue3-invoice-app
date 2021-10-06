c82ca833aaaad07761a05b41081362e0d4b039a1
Tusharshrivastav <ts3657@gmail.com>
2021-10-06 07:26:16 +0530

    added nav

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,22 @@
 <template>
-  <router-view />
+  <div>
+    <div class="app flex">
+      <Navigation />
+      <div class="app flex flex-column">
+        <router-view />
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+import Navigation from "./components/Navigation.vue";
+export default {
+  components: {
+    Navigation,
+  },
+};
+</script>
 
 <style lang="scss">
 @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap");
